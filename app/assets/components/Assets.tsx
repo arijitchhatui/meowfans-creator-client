d23888b7456b8a20a6a52addf1f0f39f562f7052
ARIJIT <arijitchhatui.me@gmail.com>
2025-09-19 23:38:18 +0530

    fix :assets scrolling

--- app/assets/components/Assets.tsx
+++ app/assets/components/Assets.tsx
@@ -8,19 +8,32 @@ import { AssetsHeader } from './Header';
 import { AssetsThread } from './Thread';
 
 export const Assets = () => {
-  const { data: assets, refetch } = useQuery(GET_CREATOR_ASSETS_QUERY, {
-    variables: { input: { limit: 50 } }
+  const {
+    data: assets,
+    refetch,
+    fetchMore
+  } = useQuery(GET_CREATOR_ASSETS_QUERY, {
+    variables: { input: { limit: 10, offset: 0 } }
   });
 
   const handleRefetch = async () => {
     await refetch();
   };
 
+  const handleLoadMore = async () => {
+    await fetchMore({
+      variables: { input: { offset: assets?.getCreatorAssets.length, limit: 10 } },
+      updateQuery: (prev, { fetchMoreResult }) => ({
+        getCreatorAssets: [...prev.getCreatorAssets, ...fetchMoreResult.getCreatorAssets]
+      })
+    });
+  };
+
   return (
     <PageWrapper>
       <AssetsHeader />
       <Separator />
-      <AssetsThread assets={assets} onUpload={handleRefetch} />
+      <AssetsThread assets={assets} onUpload={handleRefetch} onLoadMore={handleLoadMore} />
     </PageWrapper>
   );
 };
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,15 +5,16 @@ import { Button } from './ui/button';
 
 interface Props {
   message?: string;
+  onClick?: () => unknown;
 }
 
-export const Footer: React.FC<Props> = ({ message }) => {
+export const Footer: React.FC<Props> = ({ message, onClick }) => {
   return (
-    <>
-      <Div className="shadow-md p-4 flex justify-center gap-3 rounded-2xl">
-        <Button className="px-6 py-3 rounded-xl font-semibold hover:bg-gray-200">Load more</Button>
-      </Div>
+    <Div className="shadow-md p-4 flex justify-center gap-3 rounded-2xl">
+      <Button className="px-6 py-3 rounded-xl font-semibold cursor-pointer" onClick={onClick}>
+        Load more
+      </Button>
       {message && <Typography className="text-center text-sm text-gray-500 mt-2 mb-4">{message}</Typography>}
-    </>
+    </Div>
   );
 };
